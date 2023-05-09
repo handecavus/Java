672300c2ce24dede4e9eb69e0dbc073007960046
handecavus <handeecavus@gmail.com>
2023-05-09 13:53:53 +0300

    Gun02 _01_CommentPrint

---
+++ src/Gun02/_01_CommentPrint.java
@@ -0,0 +1,18 @@
+package Gun02;
+
+public class _01_CommentPrint {
+    public static void main(String[] args) {
+
+        // yorum satırı
+
+        /*
+
+        çoklu yorum satırı
+
+         */
+
+        System.out.println("Merhaba Dünya"); // ekrana yazdırma
+
+
+    }
+}
