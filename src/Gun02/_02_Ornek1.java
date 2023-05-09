bd6ae360f4da0131cf0cdc830b9fdb1167085f84
handecavus <handeecavus@gmail.com>
2023-05-09 14:02:10 +0300

    Gun02 Ornek1 ve Ornek2

---
+++ src/Gun02/_02_Ornek1.java
@@ -0,0 +1,14 @@
+package Gun02;
+
+public class _02_Ornek1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hande Çavuş");    // ln varsa yazdıktan sonra alt satıra geçer
+        System.out.println();                 // yeni satır açar
+        System.out.print("Merhaba");
+        System.out.println();
+        System.out.println("Hande \n Çavuş"); // \n -> yeni satır açar
+
+
+    }
+}
