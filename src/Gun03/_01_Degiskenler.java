fbd2bea576887f66dd7b525336fe762f6dfa75c7
handecavus <handeecavus@gmail.com>
2023-05-09 14:18:09 +0300

    Gun03 Degiskenler

---
+++ src/Gun03/_01_Degiskenler.java
@@ -0,0 +1,16 @@
+package Gun03;
+
+public class _01_Degiskenler {
+    public static void main(String[] args) {
+
+        // programda bir veri saklayacaksam (sayı gibi, sayaç gibi, toplam gibi)
+        // benim bilgisayara ne kadar yer istediğimi ve değişkenin adını
+        // söylemem lazım. int sayı -> INTENGER yani tam sayı
+
+        int sayi; // hafızada (RAM, gecici hafızada) int kadar bir yer ayırıyor
+                  // bu ayrılan yerin adına sayı denir. tip olarak da tam sayı
+
+        sayi=5;
+
+    }
+}
---
+++ src/Gun03/_02_Degiskenler.java
@@ -0,0 +1,18 @@
+package Gun03;
+
+public class _02_Degiskenler {
+    public static void main(String[] args) {
+
+        int sayi;  // bana int miktarınca bir tam sayı saklayacağım bir yer ayır adını "sayi" verdim.
+
+        sayi=5;
+
+        System.out.println("sayi"); // sayi
+        System.out.println(sayi);   // 5
+
+        System.out.print("sayi=");
+        System.out.println(sayi);
+
+        System.out.println(" sayi= " + sayi); // sayi=5
+    }
+}
