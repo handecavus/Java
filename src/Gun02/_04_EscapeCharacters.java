fbacb286a8b8120ce6f087c16507803cd7731ccc
handecavus <handeecavus@gmail.com>
2023-05-09 14:06:47 +0300

    Gun02 EscapeCharacters

---
+++ src/Gun02/_04_EscapeCharacters.java
@@ -0,0 +1,27 @@
+package Gun02;
+
+public class _04_EscapeCharacters {
+    public static void main(String[] args) {
+
+        // Escape Characters
+        //  \n -> yeni satır açıyor, alt satıra geçiyor
+        //  \t -> sanki tab tuşuna basılmış gibi boşluk veriyor
+        //  \b -> back-space bir karakter geri siler
+        //  \" -> " tırnak yazmak için kullanılıyor
+        //  \\ -> \ yazılacak ekrana
+        //  \r -> satırın başına geri ve satırı silip tekrar baştan yazıyormuş gibi
+
+        System.out.println("MerhabaDünya"); // MerhabaDünya
+        System.out.println("Merhaba\nDünya"); // Merhaba yazar alt satıra geçer Dünya
+        // print veya println \ görünce bu ekrana yazılacak bir karakter değil,
+        // ayrı bir iş yapmam isteniyor, devam eden n karakterini görünce
+        // ayrı işin alt satıra geçmek olduğunu anlıyor
+
+        System.out.println("Merhaba\tDünya");   // Merhaba   Dünya
+        System.out.println("Merhaba\bDünya");   // MerhabDünya
+        System.out.println("Merhaba\"Dünya");   // Merhaba"Dünya
+        System.out.println("Merhaba\\Dünya");   // Merhaba\Dünya
+        System.out.println("Merhaba\rDünya");   // Dünya
+
+    }
+}
