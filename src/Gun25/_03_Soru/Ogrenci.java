43e2a2703bba6eeecbcbbf06cc8e30eb6a2609f7
handecavus <handeecavus@gmail.com>
2023-05-26 13:04:06 +0300

    Gun25

---
+++ src/Gun25/_03_Soru/Ogrenci.java
@@ -0,0 +1,8 @@
+package Gun25._03_Soru;
+
+public class Ogrenci {
+    String ad;
+    String soyad;
+    int sinif;
+    String adres;
+}
---
+++ src/Gun25/_03_Soru/Okul.java
@@ -0,0 +1,29 @@
+package Gun25._03_Soru;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Okul {
+    public static void main(String[] args) {
+
+        Scanner okuInt=new Scanner(System.in);
+        Scanner okuStr=new Scanner(System.in);
+        ArrayList<Ogrenci> snf=new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            Ogrenci ogr = new Ogrenci();
+            System.out.print("Öğrenci Adı:"); ogr.ad = okuStr.nextLine();
+            System.out.print("Öğrenci Soyadı:"); ogr.soyad = okuStr.nextLine();
+            System.out.print("Öğrenci Sınıfı:"); ogr.sinif = okuInt.nextInt();
+            System.out.print("Öğrenci Adresi:"); ogr.adres=okuStr.nextLine();
+
+            snf.add(ogr);
+        }
+        for (Ogrenci ogr : snf){
+            System.out.println("ogr.ad = " + ogr.ad);
+            System.out.println("ogr.soyad = " + ogr.soyad);
+            System.out.println("ogr.sinif = " + ogr.sinif);
+            System.out.println("ogr.adres = " + ogr.adres);
+        }
+    }
+}
