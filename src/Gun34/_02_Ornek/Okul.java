99cb09a1fb7304f2e1213b902e9471df0e10a297
handecavus <handeecavus@gmail.com>
2023-06-02 19:47:08 +0300

    Gun34

---
+++ src/Gun34/_02_Ornek/Okul.java
@@ -0,0 +1,13 @@
+package Gun34._02_Ornek;
+
+public class Okul {
+    public static void main(String[] args) {
+        LiseOgrencisi lo1=new LiseOgrencisi("Hande Çavuş", OgrTip.LISE,"SAY");
+        LiseOgrencisi lo2=new LiseOgrencisi("Çağla Çavuş", OgrTip.LISE,"SÖZ");
+        ilkOgrencisi io=new ilkOgrencisi("Simge Aydın", OgrTip.ILK,"Satranç");
+
+        System.out.println("lo1 = " + lo1);
+        System.out.println("lo2 = " + lo2);
+        System.out.println("io = " + io);
+    }
+}
